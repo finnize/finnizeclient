2f33dbe7727a9a0486d408c3528cbf60b55b4369
Puwa Termnuphan <steampro35200@gmail.com>
2023-08-09 12:08:37 +0700

    updated: example files

--- example/upload_signal_example.py
+++ example/upload_signal_example.py
@@ -1,10 +1,12 @@
-from src.workflow import upload_tradingview_signal
+from finnizeclient.workflow import upload_tradingview_signal
 
-# Before uploading any signals to the Finnize website, you need to know the `strategy_id`.
-# The weight should be between 0 and 1. A value of 1 signifies a full Long or Short entry (100%).
+#   Before uploading any signals to the Finnize website, you need to know the `strategy_id`.
+#   The weight should be between 0 and 1. A value of 1 signifies a full Long or Short entry (100%).
+#   .
+#   Enter the path, strategy_id, weight and url before uploading signals
 upload_tradingview_signal(
-    path="list_of_trade_example.csv",
-    strategy_id=1,
+    path="example/list_of_trade_example.csv",
+    strategy_id=3907697156338858,
     weight=1,
     url="https://dev-client-gateway.finnize.com/api/v1/strategy-signal/",
 )
